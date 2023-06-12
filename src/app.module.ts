f7bcef2956d8c323ee1498a038ddbe31447f8b9b
alvinkingwa <alvin kingwa>
2023-06-12 10:27:11 +0300

    push changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { productsModule } from './products/products.module';
+import { ControllerModule } from './test/controller/controller.module';
 
 @Module({
-  imports: [productsModule],
+  imports: [productsModule, ControllerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/test/controller/controller.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ControllerService } from './controller.service';
+import { CreateControllerDto } from './dto/create-controller.dto';
+import { UpdateControllerDto } from './dto/update-controller.dto';
+
+@Controller('controller')
+export class ControllerController {
+  constructor(private readonly controllerService: ControllerService) {}
+
+  @Post()
+  create(@Body() createControllerDto: CreateControllerDto) {
+    return this.controllerService.create(createControllerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.controllerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.controllerService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateControllerDto: UpdateControllerDto,
+  ) {
+    return this.controllerService.update(+id, updateControllerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.controllerService.remove(+id);
+  }
+}
